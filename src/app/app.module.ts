5e1cf5e5828cff565458d17c0c3cefc71e3b2282
NaveenRK45 <naveen45.vita@email.com>
2023-04-20 18:19:20 +0530

    project  initial check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/modules/material/material.module';
 
+
 @NgModule({
   declarations: [
     AppComponent
